c488f90260350eed6644d7fb134d3b826f679013
ashwintantri <ashwin.tantri@somaiya.edu>
2018-09-30 01:17:28 +0530

    added racecar image

--- game.py
+++ game.py
@@ -1,19 +1,37 @@
 import pygame
 
+
 pygame.init()
 
-gameDisplay = pygame.display.set_mode((800,600))
+
+display_width = 800
+display_height = 600
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('RaceIT')
 
-clock = pygame.time.Clock()
+black = (0,0,0)
+white = (255,255,255)
 
+clock = pygame.time.Clock()
 crashed = False
+carImg = pygame.image.load('racecar.png')
+
+def car(x,y):
+    gameDisplay.blit(carImg, (x,y))
+
+x =  (display_width * 0.45)
+y = (display_height * 0.8)
+
 while not crashed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
 
-        print(event)
+    gameDisplay.fill(white)
+    car(x,y)
+
+        
     pygame.display.update()
     clock.tick(60)
 
